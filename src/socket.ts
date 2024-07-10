ada70bdb8f362081349bcb0c291b59ba89711c6e
Ibragimov Eldar <1ibragimov.e.i20@gmail.com>
2024-07-10 17:06:09 +0300

    fix ts errors

--- src/socket.ts
+++ src/socket.ts
@@ -10,7 +10,7 @@ export const state = reactive({
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV || "http://localhost:3000";
 
-export const socket = io("http://192.168.88.248:3000", { transports: ['websocket'] });
+export const socket = io("https://tic-tac-toe-server-v8ld.onrender.com", { transports: ['websocket'] });
 
 socket.on("connect", () => {
   state.connected = true;
